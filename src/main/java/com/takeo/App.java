74ae29f4d9cff8dbcbd94ee9b5875a547390e9fa
Ram <ram@krishna.com>
2024-01-14 18:06:51 -0500

    dvdh

--- src/main/java/com/takeo/App.java
+++ src/main/java/com/takeo/App.java
@@ -1,12 +1,13 @@
 package com.takeo;
 
-import java.sql.Date;
 import java.util.Random;
+import java.sql.Date;
+
 public class App 
 {
     public static void main( String[] args )
     {
-	System.out.println("path = "+System.getenv("path"));
-        System.out.println( "Generated random numbere n = "+new Random().nextDouble()+" on "+new Date(124, 0, 13));
+        System.out.println( "path = "+System.getenv("path"));
+	System.out.println("Random number , n = "+new Random().nextDouble()+", was generated on "+new Date(124, 0, 14));
     }
 }
